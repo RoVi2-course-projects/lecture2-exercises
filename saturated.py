a30b6ef7c2a82602979edbb3572032b8a7e39325
jlrandulfe <jlrandulfe@gmail.com>
2018-02-13 13:30:39 +0100

    Add the measure of at least one saturated pixel and all saturated pixels

--- saturated.py
+++ saturated.py
@@ -10,14 +10,19 @@ if __name__ == "__main__":
         red_sat = np.count_nonzero(image[:, :, 0] == 255)
         green_sat = np.count_nonzero(image[:, :, 1] == 255)
         blue_sat = np.count_nonzero(image[:, :, 2] == 255)
+        all_sat = np.count_nonzero((image[:, :, 0] == 255)
+                * (image[:, :, 1] == 255) * (image[:, :, 2] == 255))
+        any_sat = np.count_nonzero((image[:, :, 0] == 255)
+                + (image[:, :, 1] == 255) + (image[:, :, 2] == 255))
         # Initialize array if it is the first iteration
         if (idx == 32):
-            saturated = np.array([red_sat, green_sat, blue_sat])
+            saturated = np.array([red_sat, green_sat, blue_sat, all_sat,
+                                  any_sat])
         # Append the saturated values to the result matrix.
         else:
             saturated = np.vstack((saturated,
-                    np.array([red_sat, green_sat, blue_sat])))
+                    np.array([red_sat, green_sat, blue_sat, all_sat, any_sat])))
     # Save results to text file.
     out_file = "results.txt"
-    header = "RED    GREEN    BLUE"
+    header = "RED   GREEN   BLUE   ALL   ANY"
     np.savetxt(out_file, saturated, fmt="%i", header=header)
